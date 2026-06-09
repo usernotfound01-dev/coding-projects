62c2401d0cf9855fd1092d9fb00a0c7f12598796
david <david.adejoro.o@gmail.com>
2026-06-09 18:58:03 +0100

    six sevenn

---
+++ class6/activity1.py
@@ -0,0 +1,15 @@
+burgers=True
+hats=True
+hotdogs=True
+if hats and hotdogs and burgers:
+    print("all are true")
+else:
+    print("at least one is false")
+
+hats=0
+burgers=4
+hotdogs=0
+if hats>0 or hotdogs>0:
+    print(" either is greater than 0")
+else:
+    print("both are less than equal to 0")
