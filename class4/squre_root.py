af90e1ab869557b9ac99bd9d8791b51e5c02a6f1
david <david.adejoro.o@gmail.com>
2026-06-09 21:43:37 +0100

    ohio

---
+++ class4/squre_root.py
@@ -0,0 +1,6 @@
+num=int(input("enter the number you want to find the square root of"))
+if num>0:
+    squareroot=num**0.5
+    print("the square root of",num,"is",squareroot)
+else:
+    print("sorry you cannot find the square root of a negative number")
