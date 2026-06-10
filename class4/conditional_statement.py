a158256e3652445f71259f35c0a5e61921cf5e2c
david <david.adejoro.o@gmail.com>
2026-06-10 19:05:51 +0100

    conditional_statement

---
+++ class4/conditional_statement.py
@@ -0,0 +1,11 @@
+age=int(input("enter your age:"))
+
+
+
+
+if age<18:
+    print("you are under the age of 18!")
+elif age==18:
+    print("you are exactly 18 years old!")
+else:
+    print("you are over the age of 18 congratulations!")
