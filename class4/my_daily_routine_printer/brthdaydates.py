9375d332275286d5cb27e41b488a7027a824eb79
david <david.adejoro.o@gmail.com>
2026-06-13 16:48:23 +0100

    birthdays

---
+++ class4/my_daily_routine_printer/brthdaydates.py
@@ -0,0 +1,10 @@
+john="1969/05/15"
+luke="1970/08/20"
+david="1971/12/10"
+james="1972/03/25"
+michael="1973/11/30"
+print("John's birthday is on", john)
+print("Luke's birthday is on", luke)
+print("David's birthday is on", david)
+print("James's birthday is on", james)
+print("Michael's birthday is on", michael)
