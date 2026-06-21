4f5a85bbdcc3097a50a9c25fe14d0ff272d0c38e
david <david.adejoro.o@gmail.com>
2026-06-21 19:37:28 +0100

    alfabet

---
+++ class6/homework.py
@@ -0,0 +1,7 @@
+Alfabet="abcdefghijklmnopqrestuvwxyz"
+print("enter a character:")
+character = input()
+if character in Alfabet:
+    print("the character is in the alphabet")
+else:
+    print("the character is not in the alphabet")
---
+++ class6/practice1.py
@@ -0,0 +1,10 @@
+a=66
+b=55
+if not(a==b):
+    print("a and b are not equal")
+
+a=int(input("enter a number:"))
+if not(a%2==0):
+    print(" your number is odd")
+else:
+    print("your number is even")
