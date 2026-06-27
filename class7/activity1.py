440b0d9437b012516b8d1f0655002dab871c442d
david <david.adejoro.o@gmail.com>
2026-06-27 13:43:39 +0100

    identity operators

---
+++ class7/activity1.py
@@ -0,0 +1,20 @@
+
+bag=3
+if type(bag)is int:
+    print("bag is an integer")
+
+else:
+    print("bag is not an integer")
+
+cat=4.5
+if type(cat) is float:
+    print("cat is a float")
+else:
+    print("cat is not a float")
+
+x=20
+y=20
+if x is y:
+    print("x and y are the same")
+else:
+    print("x and y are not the same")
