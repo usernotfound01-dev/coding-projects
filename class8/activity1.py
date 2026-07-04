273518f5383336390e017eeab12b15bbc4bf2cf3
david <david.adejoro.o@gmail.com>
2026-07-04 13:46:54 +0100

    d&n

---
+++ class8/activity1.py
@@ -0,0 +1,13 @@
+v=5
+w=8
+x=55
+y=6
+z = (v+w) * x / y
+print(z)
+
+name="david"
+age=12
+if name == "david" or name == "son"and age >=15:
+    print("welcome")
+else:
+    print("goodbye")
---
+++ class8/activity2.py
@@ -0,0 +1,6 @@
+numerator=int(input("enter a numerator"))
+denominator=int(input("enter a denominator"))
+if numerator%denominator==0:
+    print("divisible")
+else:
+    print("not divisible") 
