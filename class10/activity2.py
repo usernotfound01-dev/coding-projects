f3aa0630ceb9471a3fcde39b7a066ea2dbdff714
david <david.adejoro.o@gmail.com>
2026-07-18 13:46:00 +0100

    Class10

---
+++ class10/activity2.py
@@ -0,0 +1,8 @@
+string1=input("enter a name")
+reversed=""
+
+for i in string1:
+    reversed=i+reversed
+
+print(reversed)
+
---
+++ class10/avivity1.py
@@ -0,0 +1,6 @@
+n=int(input("enter a number"))
+sum=0
+
+for i in range(0,n+1):
+    sum+=i
+print("the sum is",sum)
