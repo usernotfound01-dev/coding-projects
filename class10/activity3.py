09dec80fb918da1c0fdb59bea6d0e7c1e5561946
david <david.adejoro.o@gmail.com>
2026-07-22 09:57:21 +0100

    loop

--- class10/activity3.py
+++ class10/activity3.py
@@ -1,4 +1,15 @@
-number=int(input("enter a number"))
+print("Welcome to Vrisha's Power Calculator!")
+print("Vrisha is very good at maths.")
+print("She made this program to help her friends calculate powers.\n")
 
-for i in range(number,0,-1):
-    print(i)
+number = int(input("Enter the number: "))
+power = int(input("Enter the power: "))
+
+answer = 1
+
+for i in range(power):
+    answer = answer * number
+
+print("\nThe answer is:", answer)
+print(number, "raised to the power", power, "is", answer)
+print("\nThank you for using Vrisha's Power Calculator!")
