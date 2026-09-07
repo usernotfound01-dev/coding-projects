66281c58859e5d75e27151f545feb3346ff78436
david <david.adejoro.o@gmail.com>
2026-09-07 18:15:28 +0100

    guesss the number

--- class11/test.py
+++ class11/test.py
@@ -7,4 +7,15 @@ while guess>0:
     if num ==number:
         print("welldone you guessed it")
         break
-    elif 
+    elif num<1:
+        print("your five tries are up")
+    elif num in range(1,11):
+        print("your far off")
+    elif num in range(11,19):
+        print("your close")
+    elif num in range (19,21):
+        print("your very close")
+    elif num in range (22,30):
+        print("your very close")
+    else:
+        print("your far off")
