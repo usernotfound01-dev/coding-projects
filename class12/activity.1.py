cd6dc135b3dbfe6ee639872df61945b196bf2429
david <david.adejoro.o@gmail.com>
2026-09-15 17:50:18 +0100

    lemonadestand2

--- class12/activity.1.py
+++ class12/activity.1.py
@@ -11,3 +11,15 @@ def calculate_amount(price,cup):
     total=price*cup
     return total
 
+bill_amount=calculate_amount(price,cup)
+rounded_amount=round(bill_amount,2)
+
+print(f"total bill is {rounded_amount} :")
+pay=float(input("Enter the amount paid by the customer"))
+
+def calculate_change(bill,paid):
+    money=paid-bill
+    return money
+
+store_change=calculate_change(rounded_amount,pay)
+rounded_change=round(store_change,2)
